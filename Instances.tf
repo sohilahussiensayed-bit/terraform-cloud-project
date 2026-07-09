ec6c80fb9be40f1f8f0a4a65512ef23767d23a01
sohaila.hussien <sohaila.hussien@gmail.com>
2026-07-09 16:24:26 +0300

    Add update2

--- Instances.tf
+++ Instances.tf
@@ -2,10 +2,11 @@ resource "aws_instance" "webserver1" {
   ami           = "ami-002192a70217ac181"
   instance_type = "t2.micro"
   subnet_id = aws_subnet.private_az1.id
-  associate_public_ip_address = true
   vpc_security_group_ids = [
     aws_security_group.webserver1_sg.id
   ]
+
+  
   tags = {
     Name = "Web Server1"
   }
@@ -14,7 +15,11 @@ resource "aws_instance" "webserver2" {
   ami           = "ami-002192a70217ac181"
   instance_type = "t2.micro"
   subnet_id = aws_subnet.private_az2.id
-  associate_public_ip_address = true
+  vpc_security_group_ids = [
+  aws_security_group.webserver1_sg.id
+]
+
+  
   iam_instance_profile = aws_iam_instance_profile.profile.name
   tags = {
     Name = "Web Server2"
